3ca7c60686416d5424ddaeb4e0a2d33ff0cdeb40
mig <michaelleojacob@gmail.com>
2022-11-20 14:09:52 -0800

    trying ../views instead of /views for deployment

--- script.ts
+++ script.ts
@@ -14,10 +14,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.json());
-app.set("views", path.join(__dirname, "views"));
+// app.set("views", path.join(__dirname, "/views"));
+app.set("views", path.join(__dirname, "../views"));
 app.set("view engine", "ejs");
 
-app.use("/views", express.static(__dirname + "/views"));
+// app.use("/views", express.static(__dirname + "/views"));
+app.use("/views", express.static(__dirname + "../views"));
 app.use(express.static(path.join(__dirname, "public")));
 
 // async function main() {
