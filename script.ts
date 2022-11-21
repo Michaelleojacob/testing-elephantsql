8603396de829d58f1bff0d17788efc7c80ba28c8
mig <michaelleojacob@gmail.com>
2022-11-20 17:14:40 -0800

    had a duplicate line

--- script.ts
+++ script.ts
@@ -13,7 +13,6 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.json());
 app.set("views", path.join(__dirname, "/views"));
 // app.set("views", path.join(__dirname, "../views"));
 app.set("view engine", "ejs");
